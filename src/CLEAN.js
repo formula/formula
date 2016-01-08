b2f74c2443a3cb368f64c9a88061fb5c49afc048
JC Fisher <jcfisher007@gmail.com>
2016-01-07 22:45:18 -0800

    Add CLEAN
    Clean is a super cool function. It takes an object and removes null and
    undefined values. It depends on ISBLANK to compare the object. It uses
    a couple of reduce functions and chooses not to mutate local state.

---
+++ src/CLEAN.js
@@ -0,0 +1,8 @@
+// Take an object with falsy values and return a clean object with no falsy values
+import ISBLANK from './ISBLANK'
+
+export function CLEAN(obj) {
+  return Object.keys(obj)
+  .reduce( (a, b) => ISBLANK(obj[b]) ? a : a.concat(b), [])
+  .reduce( (a, b) => { a[b] = obj[b]; return a } , {})
+}
