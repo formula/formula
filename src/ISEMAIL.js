224201033153b1f2b174ef85146367968016e1e9
Peter Moresi <peter.moresi@gmail.com>
2016-01-12 23:07:01 -0800

    v0.6.13
    Improve ISEMAIL
    
    Regex now supports more valid email address format like:
    
    a+b@c.com
    a.b@c.com

--- src/ISEMAIL.js
+++ src/ISEMAIL.js
@@ -1,5 +1,6 @@
 
 export function ISEMAIL(email) {
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return re.test(email);
+  // credit to http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 };
