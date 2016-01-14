2933a86f5fa9e8d54fbeea43f5267b1750629786
Peter Moresi <peter.moresi@gmail.com>
2016-01-14 00:12:21 -0800

    Object.freeze
    To secure the code and ensure it cannot be changed from outside.

--- src/browserify.js
+++ src/browserify.js
@@ -1,5 +1,21 @@
 import * as FF from './index'
 
 if (window) {
-  window.FunctionFoundry = FF;
+  
+  // This is a sham that makes Object.freeze work (insecurely) in ES3 environments
+  // ES5 15.2.3.9
+  // http://es5.github.com/#x15.2.3.9
+  if (!Object.freeze) {
+      Object.freeze = function freeze(object) {
+          if (Object(object) !== object) {
+              throw new TypeError('Object.freeze can only be called on Objects.');
+          }
+          // this is misleading and breaks feature-detection, but
+          // allows "securable" code to "gracefully" degrade to working
+          // but insecure code.
+          return object;
+      };
+  }
+
+  window.FunctionFoundry = Object.freeze(FF);
 }
