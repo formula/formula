4b9e4d199fe0ada3521b57dba4ed49a8b29f306d
JC Fisher <jcfisher007@gmail.com>
2016-03-22 14:07:24 -0700

    Drop console.log from MAX

--- src/MAX.js
+++ src/MAX.js
@@ -4,7 +4,6 @@ import {ISNUMBER} from './ISNUMBER';
 export function MAX(...list) {
   return FLATTEN( list ).reduce((max, next) => {
     if (ISNUMBER(next)) {
-      console.log(max, next)
       return Math.max(max, next);
     }
 
