29f3236cd99729d8189480b87458e3e60c34693d
Peter Moresi <peter.moresi@pnmac.com>
2016-04-04 13:48:12 -0700

    Fix datevalue, serial

--- src/SERIAL.js
+++ src/SERIAL.js
@@ -5,7 +5,7 @@ import error from './ERROR';
 // Convert a date object into a serial number.
 // Credit: https://github.com/sutoiku/formula.js/
 export function SERIAL(date) {
-  if (!ISDATE(date)) return error.na
+  if (!ISDATE(date)) { return error.na }
   var diff = Math.ceil((date - d1900) / MilliSecondsInDay)
   return diff + ( diff > 59 ? 2 : 1)
 }
