f46569f7aa30083ce1cb2ec435f59ef53c9c37e6
JC Fisher <jcfisher007@gmail.com>
2016-05-04 19:48:44 -0700

    Fix build
    ES6 version was compiled to commonjs

--- dist/functionfoundry.js
+++ dist/functionfoundry.js
@@ -6,12 +6,15 @@ if (window) {
 },{"./lib/functionfoundry":2}],2:[function(require,module,exports){
 'use strict';
 
-// Copyright 2015 Peter W Moresi
-
-// Returns true when the value is a finite number.
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
+// Copyright 2015 Peter W Moresi
+
+// Returns true when the value is a finite number.
 function ISNUMBER(value) {
   return typeof value === 'number' && !isNaN(value) && isFinite(value);
 }
