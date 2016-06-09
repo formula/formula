cb398dc8ca098ba95925be71a9b0292cabe98599
Peter Moresi <peter.moresi@pnmac.com>
2016-06-08 18:53:27 -0700

    Fixes
    deferred values for branch/and/or
    fixes for numbervalue

--- src/and.js
+++ src/and.js
@@ -1,26 +1,29 @@
 // Copyright 2015 Peter W Moresi
 
+import {iserror} from './iserror'
 import error from './error'
+import {isfunction} from './isfunction'
 
 // AND reduces list of truthy values into true or false value
 export function and(...criteria) {
 
   // Reduce criteria into boolean value.
   return criteria.reduce(
+
     (acc, item) => {
 
+      // Once `false` or #error! is found always return previously value
+      if (acc === false || iserror(acc)) return acc
+
+      // find the value if a literal or deferred value
+      let val = isfunction(item) ? item() : item
+
       // return `#VALUE!` if not true, false, 1 or 0
-      if (item !== true && item !== false && item !== 1 && item !== 0) {
+      if (val !== true && val !== false && val !== 1 && val !== 0) {
         return error.value
       }
 
-      // Once `#VALUE!` is found then always return `#VALUE!`
-      if (acc === error.value) return error.value
-
-      // Once `false` is found always return `false`
-      if (acc === false) return false
-
-      // Return the current value whether true or false
-      return item === true || item === 1;
+      // Return true when value is true or 1
+      return val === true || val === 1;
     })
-  }
+}
