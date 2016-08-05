ffe4f93c2a52f9b8e5e8d7d1709b7a119a849076
Peter Moresi <peter.moresi@pnmac.com>
2016-08-05 12:08:56 -0700

    v 0.10.20 - Add join function

--- test/_index.js
+++ test/_index.js
@@ -3,7 +3,7 @@ import * as ff from '../src/_index'
 
 test('index', function(t) {
 
-  t.plan( 114 )
+  t.plan( 115 )
   t.ok( ff.abs, 'abs should be there')
   t.ok( ff.acos, 'acos should be there')
   t.ok( ff.add, 'add should be there')
@@ -61,6 +61,7 @@ test('index', function(t) {
   t.ok( ff.isref, 'isref should be there')
   t.ok( ff.istext, 'istext should be there')
   t.ok( ff.isoweeknum, 'isoweeknum should be there')
+  t.ok( ff.join, 'join should be there')
   t.ok( ff.left, 'left should be there')
   t.ok( ff.len, 'len should be there')
   t.ok( ff.lookup, 'lookup should be there')
