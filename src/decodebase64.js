6e54ca4c686cb04cdf66219058f6588989222fad
JC Fisher <jcfisher007@gmail.com>
2016-09-28 21:57:39 -0700

    0.10.29 - Extract decodebase64 out of decodejwt

---
+++ src/decodebase64.js
@@ -0,0 +1,41 @@
+export var decodebase64;
+
+if (typeof window !== 'undefined' && typeof atob !== 'undefined') {
+  decodebase64 = window.atob
+} else if (typeof module !== 'undefined' && module.exports) {
+  decodebase64 = function(input) {
+    return (new Buffer(input, 'base64')).toString()
+  }
+} else {
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+  function InvalidCharacterError(message) {
+    this.message = message;
+  }
+
+  InvalidCharacterError.prototype = new Error();
+  InvalidCharacterError.prototype.name = 'InvalidCharacterError';
+
+  decodebase64 = function(input) {
+
+    var str = String(input).replace(/=+$/, '');
+    if (str.length % 4 == 1) {
+      throw new InvalidCharacterError("'atob' failed: The string to be decoded is not correctly encoded.");
+    }
+    for (
+      // initialize result and counters
+      var bc = 0, bs, buffer, idx = 0, output = '';
+      // get next character
+      buffer = str.charAt(idx++);
+      // character found in table? initialize bit storage and add its ascii value;
+      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+        // and if not first of each 4 characters,
+        // convert the first 8 bits to one ascii character
+        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+      ) {
+        // try to find character in table (0-63, not found => -1)
+        buffer = chars.indexOf(buffer);
+      }
+      return output;
+    }
+}
