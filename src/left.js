b3542cadd0f8c1a4ed0ab0323a1d150c080717ae
Jim Moresi <moresij@gmail.com>
2017-06-16 09:16:16 -0700

    Update left.js

--- src/left.js
+++ src/left.js
@@ -5,15 +5,15 @@ import n from './n'
 import error from './error'
 
 export default function left(text, number) {
-
+  // For blank text value, return empty string.
   if (isblank(text)) {
     return ''
   }
-
+  // When number is invalid, return original value.
   if (!n(+number)) {
     return text
   }
-
+  // Return truncated string value.
   return text.substring( 0, number )
 
 }
