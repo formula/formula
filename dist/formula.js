a6960019cd0ab225743a04bd93c4a8804699fae8
JC <jcfisher007@gmail.com>
2017-06-27 13:01:55 -0700

    fix dist

--- dist/formula.js
+++ dist/formula.js
@@ -1,9 +1,6 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
+(function() {
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
@@ -4650,3 +4647,4 @@ var functions = {
 
 window.formula = functions;
 
+})();
