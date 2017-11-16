342b460023a75a5df54d335f5546330d3a139e9c
JC <jcfisher007@gmail.com>
2017-11-16 08:47:42 -0800

    Update test for acoth

--- test/acoth.js
+++ test/acoth.js
@@ -5,10 +5,8 @@ import test from 'tape';
 test('acoth', function(t) {
   t.plan(4)
   // TBD: off by not so much. does it matter?
-  //t.equal( acoth(6), 0.16823611831060645 );
-  t.equal( acoth(6), 0.16823611831060642 );
-  //t.equal( acoth(2),  0.5493061443340549 );
-  t.equal( acoth(2),  0.5493061443340548 );
+  t.equal( acoth(6), 0.16823611831060645 );
+  t.equal( acoth(2),  0.5493061443340549 );
   t.equal( acoth(NaN), error.value );
   t.equal( acoth('invalid'), error.value );
 })
