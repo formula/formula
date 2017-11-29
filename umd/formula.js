e7e064d1ac52a3b0f96ff3dcee2ec79aafefac6c
JC <jcfisher007@gmail.com>
2017-11-29 09:40:38 -0800

    v2.2.1 - Fix umd modules.

--- umd/formula.js
+++ umd/formula.js
@@ -1,12 +1,10 @@
-'use strict';
-
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 (function (global, factory) {
   (typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : global.formula = factory();
-})(undefined, function () {
+})(this, function () {
   'use strict';
 
   // Copyright 2015 JC Fisher
