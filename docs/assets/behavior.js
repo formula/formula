ebf04846cba43553661a74ec973074e0b7d6fde5
JC <jcfisher007@gmail.com>
2018-02-28 23:50:58 -0800

    Cleanup docs

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -2862,8 +2862,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "src/sort",
             "title": "sort"
           },
-          "depth": 2,
-          "outline": []
+          "depth": 2
         }, {
           "type": "file",
           "data": {
