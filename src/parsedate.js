e3398685aebb429f32247d6fde6ea99b2ba3e709
JC <jcfisher007@gmail.com>
2018-04-26 11:02:33 -0700

    v3.5.1 - add (obvious) use case to parsedate.

--- src/parsedate.js
+++ src/parsedate.js
@@ -1,12 +1,12 @@
 // Copyright 2015 JC Fisher
 
-import {JulianOffset} from './constants'
-import { ERRORTYPES as error } from './error'
-import isNaN from './isnan'
+import { JulianOffset } from "./constants";
+import { ERRORTYPES as error } from "./error";
+import isNaN from "./isnan";
+import isDate from "./isdate";
 
 // PARSEDATE converts a value into a Date object.
 export default function parsedate(val) {
-
   /* *******************
   Extracted from Social Calc
 
@@ -23,38 +23,36 @@ export default function parsedate(val) {
 
   ************************* */
   function convert_date_julian_to_gregorian(juliandate) {
-
     var L, N, I, J, K;
 
-    L = juliandate+68569;
-    N = Math.floor(4*L/146097);
-    L = L-Math.floor((146097*N+3)/4);
-    I = Math.floor(4000*(L+1)/1461001);
-    L = L-Math.floor(1461*I/4)+31;
-    J = Math.floor(80*L/2447);
-    K = L-Math.floor(2447*J/80);
-    L = Math.floor(J/11);
-    J = J+2-12*L;
-    I = 100*(N-49)+I+L;
+    L = juliandate + 68569;
+    N = Math.floor(4 * L / 146097);
+    L = L - Math.floor((146097 * N + 3) / 4);
+    I = Math.floor(4000 * (L + 1) / 1461001);
+    L = L - Math.floor(1461 * I / 4) + 31;
+    J = Math.floor(80 * L / 2447);
+    K = L - Math.floor(2447 * J / 80);
+    L = Math.floor(J / 11);
+    J = J + 2 - 12 * L;
+    I = 100 * (N - 49) + I + L;
 
-    return new Date(I, J-1, K);
-
+    return new Date(I, J - 1, K);
   }
 
   if (val instanceof Error) {
     return val;
-  } else if (typeof val === 'number') {
+  } else if (isDate(val)) {
+    return val;
+  } else if (typeof val === "number") {
     // val is assumed to be serial number.
-    return convert_date_julian_to_gregorian( Math.floor(val+JulianOffset) );
-  } else if (typeof val === 'string') {
-    var timestamp=Date.parse(val)
-    if (isNaN(timestamp))
-    {
+    return convert_date_julian_to_gregorian(Math.floor(val + JulianOffset));
+  } else if (typeof val === "string") {
+    var timestamp = Date.parse(val);
+    if (isNaN(timestamp)) {
       return error.value;
     }
     return new Date(timestamp);
   }
 
   return error.value;
-
 }
--- test/parsedate.js
+++ test/parsedate.js
@@ -1,11 +1,13 @@
-import test from 'tape';
-import parsedate from '../src/parsedate';
-import { ERRORTYPES as error } from '../src/error';
+import test from "tape";
+import { d1900 } from "../src/constants";
+import parsedate from "../src/parsedate";
+import { ERRORTYPES as error } from "../src/error";
 
-test('parsedate', function(t) {
-  t.plan(4);
-  t.deepEqual(parsedate('1/1/2000'), new Date('1/1/2000'))
-  t.deepEqual(parsedate(36526), new Date('1/1/2000'))
-  t.is(parsedate('foo'), error.value)
-  t.is(parsedate(error.value), error.value)
+test("parsedate", function(t) {
+  t.plan(5);
+  t.deepEqual(parsedate(d1900), d1900);
+  t.deepEqual(parsedate("1/1/2000"), new Date("1/1/2000"));
+  t.deepEqual(parsedate(36526), new Date("1/1/2000"));
+  t.is(parsedate("foo"), error.value);
+  t.is(parsedate(error.value), error.value);
 });
