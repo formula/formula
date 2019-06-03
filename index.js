1b39ea44aa5e195e9052edccc85150b0377b4052
FormBucket Support <support@formbucket.com>
2019-06-03 08:52:44 -0700

    v3.11.1- drop more files from npm distribution.

--- index.js
+++ index.js
@@ -2,8 +2,4 @@
 
 "use strict";
 
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./lib/formula.min.js");
-} else {
-  module.exports = require("./lib/formula.js");
-}
+module.exports = require("./lib/formula.min.js");
