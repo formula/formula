7b4ec5d78881c233da3a23a5122be4843cc293e5
JC Fisher <jcfisher007@gmail.com>
2019-11-19 22:05:01 -0800

    Update interpolate.js

--- test/interpolate.js
+++ test/interpolate.js
@@ -5,7 +5,7 @@ test("interpolate", t => {
   t.plan(2);
   t.equal(interpolate(0, 100, 0.4), 40);
   t.equal(
-    interpolate(new Date(2015, 11, 1), new Date(2015, 11, 31), 0.5).toString(),
-    "Wed Dec 16 2015 00:00:00 GMT-0800 (Pacific Standard Time)"
+    interpolate(new Date(2015, 11, 1), new Date(2015, 11, 31), 0.5).toISOString(),
+    "2015-12-16T08:00:00.000Z"
   );
 });
