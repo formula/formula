2530379869db26524939eafd8550ad7dca0a7b4c
JCFisher <jcfisher007@gmail.com>
2021-10-06 00:01:59 -0700

    fix tests

--- test/cumipmt.js
+++ test/cumipmt.js
@@ -5,5 +5,5 @@ import { ERRORTYPES as error } from "../src/error";
 test("cumipmt", function(t) {
   t.plan(2);
   t.equal(typeof cumipmt, "function");
-  t.equal(cumipmt(0.09 / 12, 30 * 12, 125000, 13, 24, 0), -11135.232130750843);
+  t.equal(cumipmt(0.09 / 12, 30 * 12, 125000, 13, 24, 0), -11135.232130750845);
 });
